9b68a347c2a32e3054e7421598b47a68a2d28495
Daukaevt <mupbam1@gmail.com>
2022-10-04 02:21:13 +0300

    user choice logic added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,80 @@
 package hexlet.code;
 
+//
+//import hexlet.gradle.project.games.Calc;
+//import hexlet.gradle.project.games.Even;
+//import hexlet.gradle.project.games.GCD;
+//import hexlet.gradle.project.games.Progression;
+//import hexlet.gradle.project.games.Prime;
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+    /**
+     * max game id.
+     */
+    private static final int MAXID = 6;
+    /**
+     * Even game id.
+     */
+    private static final int WELCOME = 1;
+    /**
+     * Even game id.
+     */
+    private static final int EVEN = 2;
+    /**
+     * Calc game id.
+     */
+    public static final int CALC = 3;
+    /**
+     * GCD game id.
+     */
+    public static final int GCD = 4;
+    /**
+     * Progression game id.
+     */
+    public static final int PROGRESSION = 5;
+    /**
+     * Progression game id.
+     */
+    public static final int PRIME = 6;
+
+    public static void main(final String[] args) {
+        makeUserDialog();
+    }
+
+    /**
+     * user dialog init.
+     */
+    public static void makeUserDialog() {
+        int intScan;
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit""");
+        Scanner sc = new Scanner(System.in);
+        while (!sc.hasNextInt()) {
+            sc.next();
+        }
+        intScan = sc.nextInt();
+        System.out.println("Your choice: " + intScan);
+        if (0 < intScan && intScan <= MAXID) {
+            switch (intScan) {
+                case WELCOME -> Cli.haveName();
+               // case EVEN -> Even.play();
+               // case CALC -> Calc.play();
+               // case GCD -> hexlet.gradle.project.games.GCD.play();
+               // case PROGRESSION -> Progression.play();
+              //  case PRIME -> Prime.play();
+                default -> {
+                }
+            }
+        }
+        sc.close();
     }
 }
