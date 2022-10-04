966dd6da2e185aa613221b1e282935991da36faf
Daukaevt <mupbam1@gmail.com>
2022-10-04 16:44:57 +0300

    utils added

--- app/build/install/app/bin/app.bat
+++ app/build/install/app/bin/app.bat
@@ -67,7 +67,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\app-1.0-SNAPSHOT.jar
+set CLASSPATH=%APP_HOME%\lib\app-1.0-SNAPSHOT.jar;%APP_HOME%\lib\MathParser.org-mXparser-5.0.7.jar
 
 
 @rem Execute app
