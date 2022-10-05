a2fae3fb6b90a3699a4d93348164113000fafa7a
Daukaevt <mupbam1@gmail.com>
2022-10-05 03:36:00 +0300

    data class added

--- app/src/main/java/hexlet/code/EngineData.java
+++ app/src/main/java/hexlet/code/EngineData.java
@@ -2,7 +2,7 @@ package hexlet.code;
 
 import java.util.List;
 
-public class EngineData {
+public final class EngineData {
 
     /**
      * game quest data.
@@ -11,26 +11,48 @@ public class EngineData {
     /**
      *  list of 3 quests.
      */
-    private List<Object> questList;
+    private List<String> questList;
     /**
      * list of 3 answers.
      */
     private List<String> answerList;
+
+    /**
+     * data class.
+     * @param quest game question.
+     * @param listQuest list of game question params.
+     * @param listAnswer list of game answers.
+     */
     public EngineData(
             final String quest,
-            final List<Object> listQuest,
+            final List<String> listQuest,
             final List<String> listAnswer
     ) {
         this.gameQuest = quest;
         this.questList = listQuest;
         this.answerList = listAnswer;
     }
+
+    /**
+     * game question.
+     * @return game question string.
+     */
     public String getGameQuest() {
         return this.gameQuest;
     }
-    public List<Object> getQuestList() {
+
+    /**
+     * list of game question params.
+     * @return quest list.
+     */
+    public List<String> getQuestList() {
         return this.questList;
     }
+
+    /**
+     * list of game answers.
+     * @return answers list.
+     */
     public List<String> getAnswerList() {
         return this.answerList;
     }
