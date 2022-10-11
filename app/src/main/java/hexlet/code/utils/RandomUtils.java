c1a8e99d711b0f33922e5e505076d749f57d5a90
Daukaevt <mupbam1@gmail.com>
2022-10-11 04:47:11 +0300

    typo fixed

--- app/src/main/java/hexlet/code/utils/RandomUtils.java
+++ app/src/main/java/hexlet/code/utils/RandomUtils.java
@@ -3,11 +3,12 @@ package hexlet.code.utils;
 import java.util.Random;
 
 public class RandomUtils {
-     /**
+    /**
      * make random int 0..MAXRND.
+     * @param maxRnd max randoms range int.
      * @return random int
      */
-    public static String makeRandom(int maxRnd) {
+    public static String makeRandom(final int maxRnd) {
         Random rnd = new Random(); //instance of random class
         return Integer.toString(rnd.nextInt(0, maxRnd));
     }
