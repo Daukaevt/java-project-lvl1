70372bbb61d6a28c5244b50c2f8d4fcadfcbed9d
Daukaevt <mupbam1@gmail.com>
2022-10-16 10:47:01 +0300

    hello utils calling added

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,6 +1,8 @@
 package hexlet.code;
 
 
+import hexlet.code.utils.HelloUtils;
+
 import java.util.Scanner;
 
 
@@ -9,18 +11,9 @@ public class Cli {
      * ask username.
      */
     public static void haveName() {
-        String welcome = welcome();
-        System.out.print(welcome);
+        HelloUtils.hello();
         Scanner sc = new Scanner(System.in);
         String scName = sc.next();
         System.out.println("Hello, " + scName + "!");
-    }
-    /**
-     * System.in scanner per user dialog game.
-     * @return welcom string.
-     */
-    public static String welcome() {
-        return "Welcome to the Brain Games!"
-                + "\nMay I have your name? ";
     }
 }
