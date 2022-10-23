01fc604d08a35c216c5736cf382f5713b7f2f735
Daukaevt <mupbam1@gmail.com>
2022-10-23 19:12:28 +0300

    research

--- app/src/main/java/hexlet/code/games/IfEvenGame.java
+++ app/src/main/java/hexlet/code/games/IfEvenGame.java
@@ -21,16 +21,22 @@ public class IfEvenGame {
     private static final String GAMECONDITION =
             "Answer 'yes' if the number is even, otherwise answer 'no'.";
     /**
+     * array of questions.
+     */
+    static String[] questionArr = new String[GAMES];
+    /**
+     * array of answers.
+     */
+    static String[] answerArr = new String[GAMES];
+    /**
      * start Even game logic.
      */
     public static void play() {
         String userName = HelloUtils.hello();
-        String[] questionList = new String[GAMES];
-        String[] answerList = new String[GAMES];
         for (int i = 0; i < GAMES; i++) {
-            questionList[i] = String.valueOf(RandomUtils.makeRandom(MAXRND));
-            answerList[i] = EvenUtils.isEven(Integer.parseInt(questionList[i]));
+            questionArr[i] = String.valueOf(RandomUtils.makeRandom(MAXRND));
+            answerArr[i] = EvenUtils.isEven(Integer.parseInt(questionArr[i]));
         }
-        GameEngine.run(userName, GAMECONDITION, questionList, answerList);
+        GameEngine.run(userName, GAMECONDITION, questionArr, answerArr);
     }
 }
