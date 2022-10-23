522a16df2e6d45b3080760f0d69572c4c2db6674
Daukaevt <mupbam1@gmail.com>
2022-10-23 09:58:33 +0300

    constructor remooved

--- app/src/main/java/hexlet/code/GameEngine.java
+++ app/src/main/java/hexlet/code/GameEngine.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 
-import java.util.List;
 import java.util.Scanner;
 
 public class GameEngine {
@@ -18,60 +17,6 @@ public class GameEngine {
      */
     private static String input = null;
     /**
-     * username.
-     */
-    private static String userNameStr;
-    /**
-     *  list of 3 quests.
-     */
-    private final List<List<Object>> questList;
-    /**
-     * list of 3 answers.
-     */
-    private final List<String> answerList;
-
-    /**
-     * data class.
-     * @param userName username.
-     * @param listQuest  list of game question params.
-     * @param listAnswer list of game answers.
-     */
-    public GameEngine(
-            final String userName,
-            final List<List<Object>> listQuest,
-            final List<String> listAnswer
-    ) {
-        this.userNameStr = userName;
-        this.questList = listQuest;
-        this.answerList = listAnswer;
-    }
-    /**
-     * game question.
-     * @return game question string.
-     */
-    public String getName() {
-        return userNameStr;
-    }
-    /**
-     * list of game question params.
-     * @return quest list.
-     */
-    public List<List<Object>> getQuestList() {
-        return this.questList;
-    }
-
-    /**
-     * list of game answers.
-     * @return answers list.
-     */
-    public List<String> getAnswerList() {
-        return this.answerList;
-    }
-    /**
-     * string builder class instance.
-     */
-    private static final StringBuilder SB = new StringBuilder();
-    /**
      * scanner class instance.
      */
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -84,32 +29,37 @@ public class GameEngine {
      * try again remark.
      */
     private static final String AGAIN = "'\nLet's try again, ";
+
     /**
      * engine quest building method.
-     * @param engine datas.
+     * @param userNameStr username.
+     * @param gameQuest game conditions.
+     * @param questions array of quests.
+     * @param answers array of answers.
      */
-    public static void run(final GameEngine engine) {
+    public static void run(
+            final String userNameStr,
+            final String gameQuest,
+            final String[] questions,
+            final String[] answers) {
+        System.out.println(gameQuest);
         for (int i = 0; i < GAMES; i++) {
-            for (Object o : engine.getQuestList().get(i)) {
-                SB.append(o);
-            }
-            System.out.print("Question: " + SB + "\nYour answer: ");
-            SB.setLength(0);
+            System.out.print("Question: " + questions[i] + "\nYour answer: ");
             if (SCANNER.hasNext()) {
                 input = SCANNER.next();
             }
-            var yep = engine.getAnswerList().get(i);
+            var yep = answers[i];
             if (input.equals(yep)) {
                 System.out.println("Correct!");
                 count++;
             } else {
                 System.out.print("'" + input + NOPE + yep
-                        + AGAIN + engine.getName() + "!");
+                        + AGAIN + userNameStr + "!");
                 break;
             }
         }
         if (count == GAMES) {
-            System.out.println("Congratulations, " + engine.getName() + "!");
+            System.out.println("Congratulations, " + userNameStr + "!");
         }
         SCANNER.close();
     }
