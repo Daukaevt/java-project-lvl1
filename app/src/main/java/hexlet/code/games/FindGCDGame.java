b1193ba312063722792846c5484a930e21c5dbd8
Daukaevt <mupbam1@gmail.com>
2022-10-23 20:29:14 +0300

    variables remooved in class

--- app/src/main/java/hexlet/code/games/FindGCDGame.java
+++ app/src/main/java/hexlet/code/games/FindGCDGame.java
@@ -7,6 +7,14 @@ import java.util.Arrays;
 
 public class FindGCDGame {
     /**
+     * first expression number.
+     */
+    private static int firstNum;
+    /**
+     * second expression number.
+     */
+    private static int secondNum;
+    /**
      * number of games.
      */
     public static final int GAMES = 3;
@@ -28,19 +36,29 @@ public class FindGCDGame {
         String[] answerList = new String[GAMES];
         for (int i = 0; i < GAMES; i++) {
             questionList[i] = makeExpression();
-            answerList[i] = makeSolution(questionList[i]);
+            answerList[i] = makeSolution();
         }
         GameEngine.run(userName, GAMECONDITION, questionList, answerList);
     }
     /**
+     * find gcd question params.
+     * @return params string.
+     */
+    private static String makeExpression() {
+        var sb = new StringBuilder();
+        firstNum = RandomUtils.makeRandom(MAXRND);
+        sb.append(firstNum);
+        sb.append(" ");
+        secondNum = RandomUtils.makeRandom(MAXRND);
+        sb.append(secondNum);
+        return sb.toString();
+    }
+    /**
      * make correct answer.
-     * @param quest find gcd question params.
      * @return solution string.
      */
-    public static String makeSolution(
-            final String quest) {
-        var str = quest.split(" ");
-        int[] arr = {Integer.parseInt(str[0]), Integer.parseInt(str[1])};
+    public static String makeSolution() {
+        int[] arr = {firstNum, secondNum};
         Arrays.sort(arr);
         var remainder = arr[0];
         do {
@@ -53,19 +71,5 @@ public class FindGCDGame {
         } while (remainder != 0);
         return String.valueOf(arr[1]);
     }
-    /**
-     * find gcd question params.
-     * @return params string.
-     */
-    private static String makeExpression() {
-        var sb = new StringBuilder();
-        int firstRND = Integer.parseInt(
-                String.valueOf(RandomUtils.makeRandom(MAXRND)));
-        sb.append(firstRND);
-        sb.append(" ");
-        int secondRND = Integer.parseInt(
-                String.valueOf(RandomUtils.makeRandom(MAXRND)));
-        sb.append(secondRND);
-        return sb.toString();
-    }
+
 }
