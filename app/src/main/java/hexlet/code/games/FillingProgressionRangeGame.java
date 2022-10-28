d107f1590dfa49aa68d548788f29e6c66316c0e6
Daukaevt <mupbam1@gmail.com>
2022-10-28 20:50:13 +0300

    getSolution method moved to result in makeExpression method

--- app/src/main/java/hexlet/code/games/FillingProgressionRangeGame.java
+++ app/src/main/java/hexlet/code/games/FillingProgressionRangeGame.java
@@ -3,8 +3,6 @@ package hexlet.code.games;
 import hexlet.code.GameEngine;
 import hexlet.code.utils.HelloUtils;
 import hexlet.code.utils.RandomUtils;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FillingProgressionRangeGame {
     /**
@@ -24,6 +22,10 @@ public class FillingProgressionRangeGame {
      */
     public static final int PR_LENGTH = 9;
     /**
+     * progression missing number.
+     */
+    private static int result;
+    /**
      * game condition question.
      */
     private static final String GAMECONDITION =
@@ -37,7 +39,7 @@ public class FillingProgressionRangeGame {
         String[] answerList = new String[GAMES];
         for (int i = 0; i < GAMES; i++) {
             questionList[i] = makeExpression();
-            answerList[i] = getProgressionNum(questionList[i]);
+            answerList[i] = String.valueOf(result);
         }
         GameEngine.run(userName, GAMECONDITION, questionList, answerList);
     }
@@ -48,12 +50,13 @@ public class FillingProgressionRangeGame {
     public static String makeExpression() {
         StringBuilder sb = new StringBuilder();
         int firstRangeNum = RandomUtils.makeRandom(MAXRND);
+        result = firstRangeNum;
         int stepRange = RandomUtils.makeRandom(MAXSTEP) + 1;
         int indexedRange = RandomUtils.makeRandom(PR_LENGTH);
         for (int i = 0; i <= PR_LENGTH; i++) {
             if (i == indexedRange) {
-                sb.append("..");
-                sb.append(" ");
+                sb.append(".. ");
+                result += indexedRange * stepRange;
             } else {
                 sb.append(firstRangeNum);
                 sb.append(" ");
@@ -61,59 +64,6 @@ public class FillingProgressionRangeGame {
             firstRangeNum += stepRange;
         }
         return sb.toString();
-    }
-    /**
-     * get missing num from progression.
-     * @param quest progression string.
-     * @return correct answer.
-     */
-    private static String getProgressionNum(final String quest) {
-        var questArray = quest.split(" ");
-        var correctAnswer = "";
-        var tempList = new ArrayList<>();
-        var index = 0;
-        for (Object o : questArray) {
-            if (!o.equals(" ")) {
-                tempList.add(o);
-            }
-        }
-        for (int i = 0; i < tempList.size(); i++) {
-            if (tempList.get(i).equals("..")) {
-                index = i;
-            }
-        }
-        correctAnswer = Integer.toString(getAnswer(index, tempList));
-        return correctAnswer;
-    }
-    private static int getAnswer(
-            final int index, final List<Object> stringArr
-    ) {
-        int intAnswer;
-        int index1;
-        int index2;
-        int length = stringArr.size() - 1;
-        switch (index) {
-            case 0 -> {
-                index2 = Integer.parseInt(String.valueOf(stringArr.get(2)));
-                index1 = Integer.parseInt(String.valueOf(stringArr.get(1)));
-                intAnswer = index1 - (index2 - index1);
-            }
-            case PR_LENGTH -> {
-                index2 = Integer
-                        .parseInt(String.valueOf(stringArr.get(length - 2)));
-                index1 = Integer
-                        .parseInt(String.valueOf(stringArr.get(length - 1)));
-                intAnswer = index1 + (index1 - index2);
-            }
-            default -> {
-                index2 = Integer
-                        .parseInt(String.valueOf(stringArr.get(index + 1)));
-                index1 = Integer
-                        .parseInt(String.valueOf(stringArr.get(index - 1)));
-                intAnswer = (index2 + index1) / 2;
-            }
-        }
-        return intAnswer;
     }
 }
 
