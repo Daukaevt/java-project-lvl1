f60fd255363cc5712b562a0c374515c0d88ac67b
Daukaevt <mupbam1@gmail.com>
2022-10-29 01:14:53 +0300

    checkstyle typo fixed

--- app/src/main/java/hexlet/code/games/FillingProgressionRangeGame.java
+++ app/src/main/java/hexlet/code/games/FillingProgressionRangeGame.java
@@ -38,14 +38,21 @@ public class FillingProgressionRangeGame {
             int firstRangeNum = RandomUtils.makeRandom(MAXRND);
             int stepRange = RandomUtils.makeRandom(MAXSTEP) + 1;
             int indexedRange = RandomUtils.makeRandom(PR_LENGTH);
-            questionList[i] = ProgressionUtils.makeExpression(firstRangeNum, stepRange, indexedRange);
-            answerList[i] = String.valueOf(getResult(firstRangeNum, stepRange, indexedRange));
+            questionList[i] = ProgressionUtils.makeExpression(
+                    firstRangeNum,
+                    stepRange,
+                    indexedRange);
+            answerList[i] = String.valueOf(
+                    getResult(firstRangeNum, stepRange, indexedRange));
         }
         GameEngine.run(userName, GAMECONDITION, questionList, answerList);
     }
 
-    private static int getResult(int firstRangeNum, int stepRange, int indexedRange) {
-        return firstRangeNum + stepRange*indexedRange;
+    private static int getResult(
+            final int firstRangeNum,
+            final int stepRange,
+            final int indexedRange) {
+        return firstRangeNum + stepRange * indexedRange;
     }
 }
 
