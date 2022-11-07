5cff532daa6c5696d4816dcc1e58fdf1a501b929
Daukaevt <mupbam1@gmail.com>
2022-11-07 12:55:08 +0300

    dublicated makeProgression method remooved

--- app/src/main/java/hexlet/code/utils/NumberUtils.java
+++ app/src/main/java/hexlet/code/utils/NumberUtils.java
@@ -13,30 +13,6 @@ public class NumberUtils {
     }
 
     /**
-     * max progression line length.
-     */
-    public static final int PR_LENGTH = 9;
-    /**
-     * missing number question params.
-     * @param firstRangeNum first number in progression.
-     * @param stepRange difference between two nearest numbers in progression.
-     * @param indexedRange index of missing number in progression.
-     * @return missing number question params string.
-     */
-    public static String makeProgressionRange(int firstRangeNum, int stepRange, int indexedRange) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= PR_LENGTH; i++) {
-            if (i == indexedRange) {
-                sb.append(".. ");
-            } else {
-                sb.append(firstRangeNum);
-                sb.append(" ");
-            }
-            firstRangeNum += stepRange;
-        }
-        return sb.toString();
-    }
-    /**
      * make correct answer.
      * @param firstNum first number of the expression.
      * @param secondNum second number of the expression.
