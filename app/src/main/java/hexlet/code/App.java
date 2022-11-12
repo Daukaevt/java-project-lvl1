6f6f9aca4c34c169f5dfd06fd4e601b5a6f20101
Daukaevt <mupbam1@gmail.com>
2022-11-12 14:56:54 +0300

    variables renamed

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,53 +1,30 @@
 package hexlet.code;
 
-
-
 import hexlet.code.games.CalculatingGame;
 import hexlet.code.games.FillingProgressionRangeGame;
 import hexlet.code.games.FindingGCDGame;
 import hexlet.code.games.EvenOddGame;
 import hexlet.code.games.ChoosingIfPrimeGame;
-
 import java.util.Scanner;
 
 public class App {
-    /**
-     * Even game id.
-     */
-    private static final int WELCOME = 1;
-    /**
-     * Even game id.
-     */
-    private static final int EVEN = 2;
-    /**
-     * Calc game id.
-     */
-    public static final int CALC = 3;
-    /**
-     * GCD game id.
-     */
-    public static final int GCD = 4;
-    /**
-     * Progression game id.
-     */
-    public static final int PROGRESSION = 5;
-    /**
-     * Progression game id.
-     */
-    public static final int PRIME = 6;
-
-    /**
+     /**
      * main method to run.
      * @param args args array.
      */
     public static void main(final String[] args) {
         makeUserDialog();
     }
-
     /**
      * user dialog init.
      */
     public static void makeUserDialog() {
+        final int welcomeId = 1;
+        final int evenId = 2;
+        final int calcID = 3;
+        final int gcdId = 4;
+        final int progressionId = 5;
+        final int primeId = 6;
         int intScan;
         System.out.println("""
                 Please enter the game number and press Enter.
@@ -65,15 +42,13 @@ public class App {
         intScan = sc.nextInt();
         System.out.println("Your choice: " + intScan);
         switch (intScan) {
-            case WELCOME -> Cli.haveName();
-            case EVEN -> EvenOddGame.play();
-            case CALC -> CalculatingGame.play();
-            case GCD -> FindingGCDGame.play();
-            case PROGRESSION -> FillingProgressionRangeGame.play();
-            case PRIME -> ChoosingIfPrimeGame.play();
-            default -> {
-                System.out.print("Exit.");
-            }
+            case welcomeId -> Cli.haveName();
+            case evenId -> EvenOddGame.play();
+            case calcID -> CalculatingGame.play();
+            case gcdId -> FindingGCDGame.play();
+            case progressionId -> FillingProgressionRangeGame.play();
+            case primeId -> ChoosingIfPrimeGame.play();
+            default -> System.out.print("Exit.");
         }
         sc.close();
     }
